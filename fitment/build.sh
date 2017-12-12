c7e464a88a04674474bee37590467492049a554f
cheerego <302821616@qq.com>
2017-12-12 22:51:23 +0800

    description:modified by xuhaigen for test date:20171212

--- fitment/build.sh
+++ fitment/build.sh
@@ -1,3 +1,6 @@
+
+git checkout master
+git pull
 docker build -t fitmentimage -f dockerfile.build .
 docker rm -vf fitmentcontainer
-docker run -d --name fitmentcontainer fitmentimage
+docker run -d --name fitmentcontainer -p 5000:5000 fitmentimage
